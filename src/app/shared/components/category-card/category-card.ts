935ec369233fe437c33657099596d6a471963bfc
Detesstables <newfastcar17@gmail.com>
2025-08-13 06:01:58 -0400

    Avances en el home

--- src/app/shared/components/category-card/category-card.ts
+++ src/app/shared/components/category-card/category-card.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { Category } from '../../models/Category';
 
 @Component({
   selector: 'app-category-card',
   imports: [],
   templateUrl: './category-card.html',
-  styleUrl: './category-card.css'
+  styleUrl: './category-card.css',
 })
 export class CategoryCard {
   // Con @Input() recibimos los datos de la categoría del componente padre
-  @Input() category: any; // Es buena idea crear una interfaz para Category
+  @Input() category!: Category; // Agregamos el operador de aserción no nulo '!' para indicar que este valor será proporcionado por el padre, ademas de que no es opcional y tambien tiene la interface
 }
